cf8ecee6a36dc5baf46b6cfb60678dc988679fa9
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-07 17:59:22 +0530

    replacing baseURL with rootURL

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/Nike/';
+    ENV.rootURL = '/Nike/';
     ENV.locationType = 'hash';
   }
 
