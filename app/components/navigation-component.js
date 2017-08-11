6d7b60652ae04d0438aa9ffb06e4d5c5602c5b5b
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-11 13:39:18 +0530

    Updated default nav state to be closed

--- app/components/navigation-component.js
+++ app/components/navigation-component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  leftSideBarLockedOpen: true,
+  leftSideBarLockedOpen: false,
   actions : {
     toggle(leftSideBarLockedOpen) {
       console.log('sdflksdfjskldfj', leftSideBarLockedOpen);
