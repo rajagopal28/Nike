e9b625c1c50b8c24b1cba4ee7c8e2bdaf4bc505a
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-11 13:41:03 +0530

    Adding add-label compoent in route and adding route actions to add new label

--- app/routes/home/labels.js
+++ app/routes/home/labels.js
@@ -3,5 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
    return this.store.findAll('label');
- }
+ },
+ getNameFromValue: function(text) {
+   if(text && text.trim().length > 0) {
+     return text.toLowerCase().split(" ").join("_");
+   }
+   return "";
+ },
+  actions : {
+    addLabel(displayText) {
+     console.log('adding label in route');
+     var newLabel = this.get("store").createRecord('label', {
+       displayName: displayText,
+       name: this.getNameFromValue(displayText),
+       addedOn: new Date().getTime()
+     });
+     newLabel.save();
+    }
+  }
 });
