4e4dd849ba5f989014ac4f4c8e25310fad2bf122
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-11 13:40:28 +0530

    Adding UI changes for add label component and bubble action to route

--- app/components/add-label.js
+++ app/components/add-label.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  displayText: null,
+  store: Ember.inject.service('store'),
+  actions: {
+    addLabel() {
+     console.log('in component add label');
+     this.sendAction('addNewLabel', this.displayText);
+     this.set('displayText', '');
+    }
+  }
 });
