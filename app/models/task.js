cea6cc505e4e8bf7f79f2c858d583226a8050189
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-12 23:33:09 +0530

    Updating task model with belongsTo

--- app/models/task.js
+++ app/models/task.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   description: DS.attr('string'),
   title: DS.attr('string'),
   dateCreated: DS.attr('number'),
-  dateModified: DS.attr('number')
+  dateModified: DS.attr('number'),
+  user: DS.belongsTo('user')
 });
