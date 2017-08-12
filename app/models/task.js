682a960f50e1cb1a917d4ff86a99542740fe3bd1
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-12 22:17:42 +0530

    Adding new model for tasks and defining relationship with users

---
+++ app/models/task.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  description: DS.attr('string'),
+  title: DS.attr('string'),
+  dateCreated: DS.attr('number'),
+  dateModified: DS.attr('number')
+});
--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   displayName: DS.attr('string'),
   passKey: DS.attr('string'),
-  joined: DS.attr('number')
+  joined: DS.attr('number'),
+  tasks: DS.hasMany('task', { async: true, inverse: null })
 });
