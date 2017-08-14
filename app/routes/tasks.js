2d1089efc3ed0c4b40adda5fc5151c9c50627ea4
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-14 12:35:39 +0530

    Brining tasks from user in session

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -2,20 +2,23 @@ import Ember from 'ember';
 import FindQuery from 'ember-emberfire-find-query/mixins/find-query';
 
 export default Ember.Route.extend(FindQuery, {
+  authentication: Ember.inject.service(),
   model() {
    if(this.get('session.uid')) {
+     let authService = this.get('authentication');
      return Ember.RSVP.hash({
-       user: this.store.query('user', {orderBy : 'uid', equalTo: this.get('session.uid')}).then(function(users){
-         return users.get('firstObject');
-       }),
-       tasks:  this.store.findAll('task')
+       tasks: new Ember.RSVP.Promise(function(resolve, reject) {
+         let user = authService.getUser();
+         resolve(user ? user.get('tasks') : {});
+         reject({error: 'cant find something'});
+       })
      });
    }
    return [];
  },
  actions: {
-   addTask(task, user) {
-     console.log('route action', task, user);
+   addTask(task) {
+     let user = this.get('authentication').getUser();
      let taskItem = this.store.createRecord('task', task);
      user.get('tasks').addObject(taskItem);
      taskItem.save().then(function(){
