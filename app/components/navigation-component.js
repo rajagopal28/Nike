85854c317e234080cabe9f07838057dbec56f7c0
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-14 11:37:21 +0530

    Fixing Navigation component routing

--- app/components/navigation-component.js
+++ app/components/navigation-component.js
@@ -1,14 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  router: Ember.inject.service('-routing'),
   leftSideBarLockedOpen: false,
+  leftSideBarOpen: false,
   actions : {
-    toggle(leftSideBarLockedOpen) {
-      console.log('sdflksdfjskldfj', leftSideBarLockedOpen);
-      this.set('leftSideBarLockedOpen', !leftSideBarLockedOpen);
+    toggle(property) {
+      this.toggleProperty(property);
     },
     logout() {
       this.sendAction('signOutAction');
+    },
+    transitionTo(route) {
+      console.log('transitioning to', route);
+      //'action (mut leftSideBarOpen)'
+      this.toggleProperty('leftSideBarLockedOpen');
+      this.get('router').transitionTo(route);
     }
   }
 });
