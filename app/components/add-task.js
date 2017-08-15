886a9b46ff48ad17fadda066e414b614df3de62f
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-08-15 10:11:41 +0530

    Updating add-task with min date and default date

--- app/components/add-task.js
+++ app/components/add-task.js
@@ -4,7 +4,9 @@ export default Ember.Component.extend({
   task: {
     title: '',
     description: '',
-    dueDate: new Date()
+    dueDateString: new Date().toISOString().slice(0, 10),
+    dueDate: new Date(),
+    minDate: new Date()
   },
   actions: {
     addTask() {
@@ -13,12 +15,14 @@ export default Ember.Component.extend({
      this.sendAction('addNewTask', {
        title: this.task.title,
        description: this.task.description,
-       dueDate: this.task.dueDate,
+       dueDate: this.task.dueDate.getTime(),
        dateCreated: now,
        dateModifited: now
      });
      this.set('task.title', '');
      this.set('task.description', '');
+     this.set('task.dueDate', new Date());
+     this.set('task.dueDateString', new Date().toISOString().slice(0, 10));
     }
   }
 });
