0341bea5281ff56e3908b5d054a1439bca6a5b50
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-04-30 15:34:08 +0530

    Adding date formatter for timeline

---
+++ app/helpers/format-date.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export function formatDate([value, ...rest], hash) {
+  var date = new Date(value);
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  var formattedValue = day + ' ' + monthNames[monthIndex] + ' ' + year;
+  return formattedValue;
+}
+
+export default Ember.Helper.helper(formatDate);
