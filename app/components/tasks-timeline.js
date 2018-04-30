77c288d34f0bb7588ff95c901a15f7f4ce458934
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-04-30 17:36:09 +0530

    Adding warning dialog on task delete

--- app/components/tasks-timeline.js
+++ app/components/tasks-timeline.js
@@ -3,12 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   imageURLOdd : 'https://codyhouse.co/demo/vertical-timeline/img/cd-icon-picture.svg',
   imageURLEven: 'https://codyhouse.co/demo/vertical-timeline/img/cd-icon-movie.svg',
+  selectedTask: null,
+  showDeleteWarning: false,
+  deleteTaskWarningContent: 'Are you sure want to delete this task?',
   actions: {
     deleteTask(task) {
      console.log('in component delete task', this.task);
      let now = new Date().getTime();
      this.sendAction('removeTask', task);
      console.log('Sent action to delete task...');
-    }
+   },
+   deleteTaskWarning(task) {
+     this.set('selectedTask', task);
+     this.set('showDeleteWarning', true);
+   },
+   hideDialog() {
+     this.set('showDeleteWarning', false);
+   }
   }
 });
