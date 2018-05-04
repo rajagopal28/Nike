27d41430b1f185e03c0973dcca181e06236f3959
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-05-04 14:07:27 +0530

    Adding logic to add labels as chips

--- app/components/add-task.js
+++ app/components/add-task.js
@@ -2,21 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   task: null,
+  labels:[],
   didInsertElement() {
     var task = {
       title: '',
       description: '',
       dueDateString: new Date().toISOString().slice(0, 10),
+      labels: Ember.A(),// array
       dueDate: new Date(),
       minDate: new Date()
     };
     if(this.inputTask) {
       console.log('inside if...')
       var dueDate =  new Date(this.get('inputTask.dueDate'));
+      console.log(this.get('inputTask.labels'));
+      let labels = this.get('inputTask.labels').map((la) => {
+        return {name: la};
+      });
+      console.log('something', labels);
       task = {
         title: this.get('inputTask.title'),
         description: this.get('inputTask.description'),
         dueDateString: dueDate.toISOString().slice(0, 10),
+        labels: labels,
         dueDate: dueDate,
         minDate: dueDate
       };
@@ -27,17 +35,44 @@ export default Ember.Component.extend({
     addTask() {
      console.log('in component add task', this.task);
      let now = new Date().getTime();
+     let labels = this.get('task.labels').map((la) => la.get('name'));
+     console.log('maped label names', labels)
      this.sendAction('addNewTask', {
        title: this.task.title,
        description: this.task.description,
        dueDate: this.task.dueDate.getTime(),
+       labels: labels,
        dateCreated: now,
        dateModifited: now
      });
      this.set('task.title', '');
      this.set('task.description', '');
+     this.set('task.labels', []);
      this.set('task.dueDate', new Date());
      this.set('task.dueDateString', new Date().toISOString().slice(0, 10));
-    }
+   },
+   addLabel(label) {
+     if(this.task.labels.indexOf(label) === -1) {
+       var labels = this.task.labels;
+       labels.pushObject({ name: label.get('name')});
+       this.set('task.labels', labels);
+       console.log('Added label ..', label.get('name'), this.task.labels.length);
+     } else {
+       console.log('Label already added....')
+     }
+   },
+   removeLabel(label) {
+       console.log('removing label', label);
+       let targetIndex = this.task.labels.indexOf(label);
+       if(targetIndex !== -1) {
+         var labels = this.task.labels;
+         // labels.arrayContentWillChange(0, labels.length-1, labels.length-1);
+         labels.removeObject(label);
+         // labels.arrayContentWillChange(0, labels.length-1, labels.length-2);
+         this.set('task.labels', labels);
+       } else {
+         console.log('Label should be added....')
+       }
+   }
   }
 });
