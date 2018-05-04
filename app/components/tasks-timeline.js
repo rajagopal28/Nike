2704ce9226e3a240339513b3e33b216c7c8ac3ba
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-05-04 16:03:37 +0530

    Adding filtering tasks in timeline

--- app/components/tasks-timeline.js
+++ app/components/tasks-timeline.js
@@ -6,7 +6,28 @@ export default Ember.Component.extend({
   selectedTask: null,
   showDeleteWarning: false,
   showEditTaskDialog: false,
+  filteredTasks : Ember.A(),
+  filterLabels: Ember.A(),
   deleteTaskWarningContent: 'Are you sure want to delete this task?',
+  didInsertElement() {
+    this.filterTasksWithLabels();
+  },
+  filterTasksWithLabels(){
+    let filteredTasks = this.tasks;
+    let _self = this;
+    console.log('this.filterLabels', this.get('filterLabels').toArray());
+    if(this.filterLabels.length > 0) {
+      let mappedFilter = _self.get('filterLabels').map((a) => a.name);
+      filteredTasks = this.get('tasks').filter((ta) =>  _self.arrayContainsArray(ta.get('labels') || [], mappedFilter));
+    }
+    this.set('filteredTasks', filteredTasks);
+  },
+  arrayContainsArray (superset, subset) {
+    if (0 === subset.length || 0 === superset.length) {
+      return false;
+    }
+    return subset.every((value) => superset.indexOf(value) >= 0);
+  },
   actions: {
     deleteTask(task) {
      console.log('in component delete task', this.task);
@@ -34,6 +55,32 @@ export default Ember.Component.extend({
      console.log('selected task content', this.selectedTask);
      this.sendAction('updateTask', this.selectedTask, taskContent);
      this.set('showEditTaskDialog', false);
+   },
+   addLabel(label) {
+     if(this.filterLabels.indexOf(label) === -1) {
+       var labels = this.filterLabels;
+       labels.pushObject({ name: label.get('name')});
+       this.set('filterLabels', labels);
+       console.log('Added label ..', label.get('name'), this.filterLabels.length);
+     } else {
+       console.log('Label already added....')
+     }
+   },
+   removeLabel(label) {
+       console.log('removing label', label);
+       let targetIndex = this.filterLabels.indexOf(label);
+       if(targetIndex !== -1) {
+         var labels = this.filterLabels;
+         // labels.arrayContentWillChange(0, labels.length-1, labels.length-1);
+         labels.removeObject(label);
+         // labels.arrayContentWillChange(0, labels.length-1, labels.length-2);
+         this.set('filterLabels', labels);
+       } else {
+         console.log('Label should be added....')
+       }
+   },
+   applyFilter() {
+     this.filterTasksWithLabels();
    }
   }
 });
