a11c1a86c369c0ce5288d7a8805ff7984a4ea1a6
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-05-07 16:11:54 +0530

    adding model for memo/log and their relations

--- app/models/memo.js
+++ app/models/memo.js
@@ -3,10 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr('string'),
   title: DS.attr('string'),
+  dueDate: DS.attr('number'),
   dateCreated: DS.attr('number'),
   dateModified: DS.attr('number'),
-  dueDate: DS.attr('number'),
-  labels: DS.attr(),
-  user: DS.belongsTo('user', { async: true, inverse: null }),
-  logs: DS.hasMany('memo', { async: true, inverse: null })
+  task: DS.belongsTo('task', { async: true, inverse: null })
 });
