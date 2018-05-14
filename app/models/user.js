5328bd25eb4b25b4e353f17ae53e83755d010fbd
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-05-14 17:46:40 +0530

    Updating code to view users as card

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   displayName: DS.attr('string'),
   passKey: DS.attr('string'),
   joined: DS.attr('number'),
+  uid: DS.attr('string'),
   tasks: DS.hasMany('task', { async: true, inverse: null })
 });
