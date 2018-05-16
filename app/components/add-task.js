7a07652416b79c16dabc2d69d7fc8e0d3147c6e9
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2018-05-16 12:49:33 +0530

    Adding a flag to show/hide card title in add-task

--- app/components/add-task.js
+++ app/components/add-task.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
   task: null,
   labels:[],
   memoMode: false,
+  showTitle: false,
   statusList: Ember.A(),
   taskService: service('task-service'),
   init() {
